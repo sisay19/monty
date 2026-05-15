0e3dc66c24b6d30405b26e566fe8a14f9f8e9e66
sisay19 <sisayfikru69@gmail.com>
2026-05-15 03:58:03 -0700

    Add sub opcode

--- execute.c
+++ execute.c
@@ -26,6 +26,8 @@ void execute_instruction(char *opcode, char *arg, stack_t **stack,
 		add(stack, line_number);
 	else if (strcmp(opcode, "nop") == 0)
 		nop(stack, line_number);
+	else if (strcmp(opcode, "sub") == 0)
+		sub(stack, line_number);
 	else
 	{
 		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
--- opcodes.c
+++ opcodes.c
@@ -134,3 +134,23 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+/**
+ * sub - subtracts the top element from the second top element of the stack
+ * @stack: double pointer to the top of the stack
+ * @line_number: current line number
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n = (*stack)->n - temp->n;   /* second top minus top */
+	(*stack)->prev = NULL;
+	free(temp);
+}
