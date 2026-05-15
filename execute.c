437407d61dcf40787deee44fbde103671aa1d9fb
sisay19 <sisayfikru69@gmail.com>
2026-05-15 02:20:39 -0700

    Add pop opcode

--- execute.c
+++ execute.c
@@ -18,6 +18,8 @@ void execute_instruction(char *opcode, char *arg, stack_t **stack,
 		pall(stack, line_number);
 	else if (strcmp(opcode, "pint") == 0)
                 pint(stack, line_number);
+	else if (strcmp(opcode, "pop") == 0)
+		pop(stack, line_number);
 	else
 	{
 		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
--- opcodes.c
+++ opcodes.c
@@ -60,3 +60,23 @@ void pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", (*stack)->n);
 }
+/**
+ * pop - removes the top element of the stack
+ * @stack: double pointer to the top of the stack
+ * @line_number: current line number
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(temp);
+}
