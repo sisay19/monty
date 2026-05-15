9908d948e94328146e13801589e7b84e7ce1dbdc
sisay19 <sisayfikru69@gmail.com>
2026-05-15 04:32:44 -0700

    Add pchar opcode

--- execute.c
+++ execute.c
@@ -34,6 +34,8 @@ void execute_instruction(char *opcode, char *arg, stack_t **stack,
 		mul(stack, line_number);
 	else if (strcmp(opcode, "mod") == 0)
 		mod(stack, line_number);
+	else if (strcmp(opcode, "pchar") == 0)
+		pchar(stack, line_number);
 	else
 	{
 		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
--- opcodes.c
+++ opcodes.c
@@ -225,3 +225,25 @@ void mod(stack_t **stack, unsigned int line_number)
 	(*stack)->prev = NULL;
 	free(temp);
 }
+/**
+ * pchar - prints the char at the top of the stack
+ * @stack: double pointer to the top of the stack
+ * @line_number: current line number
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	int c;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	c = (*stack)->n;
+	if (c < 0 || c > 127)
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", c);
+}
