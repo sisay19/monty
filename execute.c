b0c6055e6a5921d06c4b07b0ab072703ee32b606
sisay19 <sisayfikru69@gmail.com>
2026-05-15 04:38:18 -0700

    Add pstr opcode

--- execute.c
+++ execute.c
@@ -36,6 +36,8 @@ void execute_instruction(char *opcode, char *arg, stack_t **stack,
 		mod(stack, line_number);
 	else if (strcmp(opcode, "pchar") == 0)
 		pchar(stack, line_number);
+		else if (strcmp(opcode, "pstr") == 0)
+		pstr(stack, line_number);
 	else
 	{
 		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
--- opcodes.c
+++ opcodes.c
@@ -247,3 +247,22 @@ void pchar(stack_t **stack, unsigned int line_number)
 	}
 	printf("%c\n", c);
 }
+/**
+ * pstr - prints the string starting at the top of the stack
+ * @stack: double pointer to the top of the stack
+ * @line_number: current line number (unused)
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = *stack;
+
+	(void)line_number;
+	while (current != NULL)
+	{
+		if (current->n <= 0 || current->n > 127)
+			break;
+		printf("%c", current->n);
+		current = current->next;
+	}
+	printf("\n");
+}
